4b192237bfc14a1d6a8d3042a7699217958e0a38
Ilya <37002484+n0na7773@users.noreply.github.com>
2020-12-25 04:22:44 +0200

    Update mx_strdel.c

--- src/mx_strdel.c
+++ src/mx_strdel.c
@@ -6,15 +6,4 @@ void mx_strdel(char **str) {
     *str = NULL;                 
 }
 
-/*int main() {
-    char *arr1 = malloc(7);
-    char *arr = "Hello!";
-    int i=0;
-    while (arr1[i] !=  '\0') {
-        arr1[i] = arr[i];
-        i++;
-    }
-    mx_strdel(&arr1);
-    printf("%s", arr1);
-}*/
 
